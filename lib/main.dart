20a40c8cd332d4e682711f13538d2d0fe8f88f1a
Sean.Xiao <jxzsxsp@qq.com>
2018-07-15 19:07:14 +0800

    layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,143 +1,106 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(new Demo());
 
 class Demo extends StatelessWidget {
+  Widget titleSection = Container(
+    padding: const EdgeInsets.all(32.0),
+    child: Row(
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  "Oeschinen Lake Campground",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              Text(
+                "Kandersteg, Switzerland",
+                style: TextStyle(color: Colors.grey[500]),
+              )
+            ],
+          ),
+        ),
+        Icon(Icons.star, color: Colors.red[500]),
+        Text("41"),
+      ],
+    ),
+  );
+
+  Widget textSection = Container(
+    padding: const EdgeInsets.all(32.0),
+    child: new Text(
+      '''
+Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
+        ''',
+      softWrap: true,
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
+
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w500,
+                color: color,
+              ),
+            ),
+          )
+        ],
+      );
+    }
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          buildButtonColumn(Icons.phone, "CALL"),
+          buildButtonColumn(Icons.near_me, "ROUTE"),
+          buildButtonColumn(Icons.share, "SHARE"),
+        ],
+      ),
+    );
 
     return new MaterialApp(
       title: "Demo",
       theme: new ThemeData(
-        primaryColor: Colors.green,
+        primaryColor: Colors.blue,
       ),
-      home: new RandomWords(),
-    );
-  }
-
-}
-
-class RandomWords extends StatefulWidget {
-
-  @override
-  createState() => new RandomWordsState();
-
-}
-
-class RandomWordsState extends State<RandomWords> {
-
-  final _suggestions = <WordPair>[];
-  final _saved = new Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Startup Name Generator"),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
-        ],
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
-        // 在偶数行，该函数会为单词对添加一个ListTile row.
-        // 在奇数行，该行书湖添加一个分割线widget，来分隔相邻的词对。
-        // 注意，在小屏幕上，分割线看起来可能比较吃力。
-        itemBuilder: (context, i) {
-          // 在每一列之前，添加一个1像素高的分隔线widget
-          if (i.isOdd) return new Divider();
-
-          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
-          // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
-          final index = i ~/ 2;
-          // 如果是建议列表中最后一个单词对
-          if (index >= _suggestions.length) {
-            // ...接着再生成10个单词对，然后添加到建议列表
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        }
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final _alreadySaved = _saved.contains(pair);
-
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: new Icon(
-        _alreadySaved?Icons.favorite:Icons.favorite_border,
-        color: _alreadySaved?Colors.red:null,
-      ),
-      onTap: (){
-        setState(() {
-          if(_alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      new MaterialPageRoute(
-          builder: (context) {
-            final tiles = _saved.map(
-                (pair) {
-                  final _alreadySaved = _saved.contains(pair);
-
-                  return new ListTile(
-                    title: new Text(
-                      pair.asPascalCase,
-                      style: _biggerFont,
-                    ),
-                    trailing: new Icon(
-                      _alreadySaved?Icons.favorite:Icons.favorite_border,
-                      color: _alreadySaved?Colors.red:null,
-                    ),
-                    onTap: (){
-                      setState(() {
-                        if(_alreadySaved) {
-                          _saved.remove(pair);
-                        } else {
-                          _saved.add(pair);
-                        }
-                      });
-                    },
-                  );
-                }
-            );
-
-            final divided = ListTile.divideTiles(
-                context: context,
-                tiles: tiles
-            ).toList();
-
-            return new Scaffold(
-              appBar: new AppBar(
-                title: new Text("Saved Suggesttions"),
-              ),
-              body: new ListView(children: divided),
-            );
-          },
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Top Lakes"),
+        ),
+        body: new ListView(
+          children: [
+            new Image.asset(
+              "images/lake.jpg",
+              width: 600.0,
+              height: 240.0,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
       ),
     );
   }
-
 }
