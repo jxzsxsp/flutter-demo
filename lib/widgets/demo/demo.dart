1687008ccd831a08cb7d3e604cc5ac66d645ef87
Sean.Xiao <jxzsxsp@qq.com>
2018-07-16 11:51:13 +0800

    简化入口文件，项目分模块调用

---
+++ lib/widgets/demo/demo.dart
@@ -0,0 +1,4 @@
+library demo;
+
+export 'layout_demo.dart';
+export 'words_demo.dart';
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:demo/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new Demo());
+    //await tester.pumpWidget(new Demo());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
